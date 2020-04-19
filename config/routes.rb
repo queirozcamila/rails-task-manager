70d304c437687f54af51cb6c9c3a893442682527
camila queiroz <queiroz.cami@outlook.com>
2020-04-19 12:12:09 -0300

    Adding Rails Task Manager Exercise - Not completed

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-end
+    get    "tasks",          to: "tasks#index"
+
+    get    "tasks/new",      to: "tasks#new", as: :task_new
+    post   "tasks",          to: "tasks#create"
+
+    # NB: The `show` route needs to be *after* `new` route.
+    get    "tasks/:id",      to: "tasks#show", as: :task
+
+    get    "tasks/:id/edit", to: "tasks#edit", as: :task_edit
+    patch  "tasks/:id",      to: "tasks#update"
+
+    delete "tasks/:id",      to: "tasks#destroy"
+  end
